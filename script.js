4e8d65b4ce52049fe205e7fb31b95ca1feb06b24
OlegFC <grinenkofreshcode@gmail.com>
2022-11-26 17:57:40 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const userAge = 24;
+let minAge = 18;
+
+if (userAge >= minAge) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+
+userAge >= minAge ? console.log("yes") : console.log("no");
+
+let number = 46;
+if (number % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+
+const userAge1 = 14;
+if ((userAge1 >= 6) && (userAge1 < 18)) {
+    console.log('child');
+} else {
+    console.log('adult');
+}
