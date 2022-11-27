25e691808be482a7f051c0b264d422a0714d7eaf
OlegFC <grinenkofreshcode@gmail.com>
2022-11-27 16:32:28 +0200

    Add own work if else

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Урок по if else
+
 const userAge = 24;
 let minAge = 18;
 
@@ -17,8 +19,34 @@ if (number % 2 === 0) {
 }
 
 const userAge1 = 14;
-if ((userAge1 >= 6) && (userAge1 < 18)) {
-    console.log('child');
+if (userAge1 >= 6 && userAge1 < 18) {
+  console.log("child");
 } else {
-    console.log('adult');
+  console.log("adult");
+}
+
+const number1 = 58;
+const positiveNum = 0;
+if (number1 >= positiveNum) {
+  console.log("positive");
+} else {
+  console.log("negative");
+}
+
+let num1 = 12;
+let num2 = 32;
+const result1 = num1 - num2;
+const result2 = num1 * num2;
+
+if (num1 > num2) {
+  console.log(result1);
+} else {
+  console.log(result2);
+}
+
+const num3 = 21;
+if (num3 % 7 === 0 && num3 % 3 === 0) {
+  console.log("you win");
+} else {
+  console.log("you loose");
 }
