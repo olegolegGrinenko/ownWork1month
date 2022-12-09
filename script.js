d51a0ffd8795fdb606490211e40c528ede401f33
OlegFC <grinenkofreshcode@gmail.com>
2022-12-09 20:34:19 +0200

    learn function part 2

--- script.js
+++ script.js
@@ -1,232 +1,163 @@
-/*// Урок по function
-
-/**
- * 
- * @param {number} value 
- * @returns {number}
- */
-
-/*function checkInput(value = 23) {
-  if(value % 2 === 0) {
-    return 'even';
-  }
-  return 'odd';
-} 
-console.log(checkInput()); 
-
-// задача 1
-debugger
-let userInput = prompt ('Enter number: ');
-
-function checkUserInputNumber(value) {
-  if (value === "" || value === null || isNaN(value) || value === " ") {
-    return false;
-  }
-  return true;
-}
-
-if (checkUserInputNumber(userInput)) {
-  const finalUserInput = Number(userInput);
-  console.log(finalUserInput); 
-} else {
-  console.log('try again');
-}
-
-/**
- * 
- * @param {number} value 
- * @returns {string}
- */
-
-/* function checkUserInput(value = userInput) {
-  if(value % 2 === 0) {
-    return 'even';
-  }
-  return 'odd';
-} 
-console.log(checkUserInput());
-
-
-
-// Задача №2
-//debugger;
-
-function giveResult(_value1 = 1, _value2 = "2") {
-  if (typeof _value1 === typeof _value2 && _value1 > _value2) {
-    return _value1;
-  } else if (typeof _value1 === typeof _value2 && _value2 > _value1) {
-    return _value2;
-  }
-  return null;
-}
-console.log(giveResult());
-
-// function day №2
-
-// функцыя яка обчислюе периметр квадрата.
-
-let userInput = prompt("Enter value: ");
-
-function checkUserInputValue(value) {
-  if (value === "" || value === null || isNaN(value) || value === " ") {
-    return false;
-  }
-  return true;
-}
-
-if (checkUserInputValue(userInput)) {
-  const finalUserInput = Number(userInput);
-  console.log(culcPerimetr());
-} else {
-  console.log("try again");
-}
-
-
-
-function culcPerimetr(value1 = userInput, value2 = 4) {
-  return value1 * value2;
-}
-
-// задача №1
-//debugger;
-let userInput1 = prompt("Enter num1: ");
-
-const leadToNumber = function (value) {
-  if (value === "" || value === null || isNaN(Number(value)) || value === " ") {
-    return "error";
-  }
-  return Number(value);
-};
-console.log(leadToNumber(userInput1));
-
-const checkUserInput1 = function (num1) {
-  if (isNaN(num1)) {
-    return "try again";
-  } else if (num1 % 2 === 0) {
-    return "even";
-  }
-  return "odd";
-};
-const userInputNumber = leadToNumber(userInput1);
-console.log(checkUserInput1(userInputNumber));
-
-// задача №2
-const adultAge = 18;
-
-const isPersonAdult = function (age) {
-  if (age >= adultAge) {
-    return true;
-  }
-  return false;
-};
-console.log(isPersonAdult());
-
-
-const isPersonAdult = function (age, AGE_ADULT = 18) {
-  return age >= AGE_ADULT;
-}
-console.log(isPersonAdult(1));
-*/
-
-// задача №3
+// task1
 /*
-const checkMultiplicity = function (val1, val2) {
-  return val1 % val2 === 0 
+const getTextAdultOrNoAdult = function (value, ADULT_AGE = 18) {
+  return value >= ADULT_AGE ? "adult" : "no adult";
 };
-console.log(checkMultiplicity()); 
+console.log(getTextAdultOrNoAdult(16));
 
-
-const checkTriangle = function (side1, side2, side3) {
-  return (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1);
-};
-console.log(checkTriangle());
-
-// написати ф-я, яка описую периметр квадрату
-//debugger
-const userInputSide = prompt("Enter side:");
-
-const checkInput = function (value) {
-  if (value === "" || value === null || value === " " || isNaN(Number(value))) {
-    return NaN;
+// ф-я всередины себе запытуэ у користувача ввод будь-чого ы повертаэ текст thanks , або try again.
+debugger
+const getTekstByUserInput = function () {
+  const userInput = prompt("enter something");
+  if (userInput === "" || userInput === null) {
+    return "try again";
   }
-  return Number(value);
+  return "thanks";
 };
 
-/**
- * calculate and return perimetr
- * @param {number} side 
- * @returns {number | null}
+console.log(getTekstByUserInput());
 
-const getPerimetrSquare = function (side) {
-  if (isNaN(side) || side <= 0) {
-    return null;
+// task 2 switch
+
+const season = prompt("enter month");
+
+switch (season) {
+  case "12":
+  case "1":
+  case "2":
+    console.log("winter");
+    break;
+  case "3":
+  case "4":
+  case "5":
+    console.log("spring");
+    break;
+  case "6":
+  case "7":
+  case "8":
+    console.log("summer");
+    break;
+  case "9":
+  case "10":
+  case "11":
+    console.log("autumn");
+    break;
+  default:
+    console.log("404 try again");
+    break;
+}
+
+// task 2 else if
+const season1 = prompt("enter month");
+
+if (season1 === "12" || season1 === "1" || season1 === "2") {
+  console.log("winter");
+} else if (season1 === "3" || season1 === "4" || season1 === "5") {
+  console.log("spring");
+} else if (season1 === "6" || season1 === "7" || season1 === "7") {
+  console.log("summer");
+} else if (season1 === "8" || season1 === "9" || season1 === "10") {
+  console.log("autumn");
+} else {
+  console.log("404 try again");
+}
+
+//task3 else if
+
+const userAge = Number(prompt("enter your age"));
+
+if (userAge >= 0 && userAge < 2) {
+  console.log("baby");
+} else if (userAge < 6) {
+  console.log("kinder");
+} else if (userAge < 12) {
+  console.log("child");
+} else if (userAge < 18) {
+  console.log("teenager");
+} else if (userAge < 65) {
+  console.log("adult");
+} else if (userAge < 100) {
+  console.log("old man");
+} else if (userAge < 150) {
+  console.log("supper");
+} else {
+  console.log("404 try again");
+}
+
+// task 4 else if
+//debugger;
+const userTime = Number(prompt("enter your time"));
+
+const checkUserTime = function (value) {
+  if (value === "" || value === " " || value === null || isNaN(value)) {
+    return "error";
   }
-  return side + side + side + side;
+  return value;
 };
 
-console.log(getPerimetrSquare(checkInput(userInputSide)));
-*/
-// написати ф-ю, которая проверяет возраст пользователя на полнолетие
-
-const isUserAdult = function (age, ADULT_AGE = 18) {
-  return age >= ADULT_AGE;
-};
-console.log(isUserAdult(18));
-
-//ф-я принимает 2 параметра и проверяет кратность первого вторым.
-
-/**
- * multiplicity check
- * @param {number} val1
- * @param {number} val2
- * @returns {number}
- */
-const checkMultiplicity = function (val1, val2) {
-  return val1 % val2 === 0;
-};
-console.log(checkMultiplicity(20, 4));
-
-// написати 3 числа та перевирити чи можуть бути ци числа сторонами трикутника.
-
-const isSideTriangle = function (side1, side2, side3) {
-  return (
-    side1 + side2 > side3 && side3 + side2 > side1 && side1 + side3 > side2
-  );
-};
-console.log(isSideTriangle());
-
-// написать ф-ю, которая принимает 2 аргумента и если они одного типа, то возврашает тот который имеет большее значение ( если значения равны, то возвращает null), в другом случае (если разного типа) тоже возвращает null.
-
-const checkOnevalueBiggerAnother = function (value1, value2) {
-  if ((value1 > value2) && (typeof value1 === typeof value2)) {
-    return value1;
-  } else if ((value2 > value1) && typeof (value1 === typeof value2)) {
-    return value2;
+const returnToUserGreetings = function (time) {
+  if (time > 6 && time < 11) {
+    return "good motning";
+  } else if (time >= 11 && time < 18) {
+    return "good day";
+  } else if (time >= 18 && time < 23) {
+    return "good evening";
+  } else if (time >= 23 || time <= 6) {
+    return "good night";
   } else {
-    return null;
+    return "404 try again";
   }
-  
 };
-console.log(checkOnevalueBiggerAnother(2, 7));
 
-//написати функцію, яка повертає слово 'odd' або 'even', в залежності від прийнятого аргументу(число).Значення для перевірки функції вводить користувач.
+console.log(returnToUserGreetings(checkUserTime(userTime)));
+*/
+//task 4 switch
 
-const userInput = prompt ('enter number: ');
+const userTime1 = Number(prompt("enter your time"));
 
-const checkUserInput = function (value) {
-  if ((value === "") || (value === " ") || (value === null) || (isNaN(value))) {
+const checkUserTime1 = function (value) {
+  if (value === "" || value === " " || value === null || isNaN(value)) {
     return "error";
   }
-  return Number(value); 
+  return value;
 };
 
-const checkOddorEven = function (value) {
-  if (isNaN(value)) {
-    return "error";
-  } else if (value % 2 === 0) {
-    return 'even';
+switch (checkUserTime1(userTime1)) {
+  case 23:
+  case 0:
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    console.log("good night");
+    break;
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+    console.log("good morning");
+    break;
+  case 11:
+  case 12:
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+    console.log("good day");
+    break;
+  case 18:
+  case 19:
+  case 20:
+  case 21:
+  case 22:
+    console.log("good evening");
+    break;
+  default:
+    console.log("404 try again");
+    break;
   }
-  return 'odd';
-};
 
-console.log(checkOddorEven(checkUserInput(userInput)));
+//task1  
